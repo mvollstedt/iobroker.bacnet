53049dca41ab0696880706af74ad7aefa831c349
mvollstedt <101109523+mvollstedt@users.noreply.github.com>
2025-08-08 14:49:59 +0200

    Add files via upload

---
+++ admin/words.js
@@ -0,0 +1,46 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+	'bacnet adapter settings': {
+		'en': 'Adapter settings for bacnet',
+		'de': 'Adaptereinstellungen für bacnet',
+		'ru': 'Настройки адаптера для bacnet',
+		'pt': 'Configurações do adaptador para bacnet',
+		'nl': 'Adapterinstellingen voor bacnet',
+		'fr': "Paramètres d'adaptateur pour bacnet",
+		'it': "Impostazioni dell'adattatore per bacnet",
+		'es': 'Ajustes del adaptador para bacnet',
+		'pl': 'Ustawienia adaptera dla bacnet',
+		'uk': 'Налаштування адаптера для bacnet',
+		'zh-cn': 'bacnet的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option1',
+		'ru': 'вариант1',
+		'pt': 'opção1',
+		'nl': 'Option1',
+		'fr': 'option1',
+		'it': 'opzione1',
+		'es': 'Opción1',
+		'pl': 'opcja 1',
+		'uk': 'Варіант1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option2',
+		'ru': 'вариант2',
+		'pt': 'opção2',
+		'nl': 'Optie2',
+		'fr': 'Option2',
+		'it': 'opzione2',
+		'es': 'opción 2',
+		'pl': 'opcja2',
+		'uk': 'Варіант2',
+		'zh-cn': '选项2'
+	}
+};
